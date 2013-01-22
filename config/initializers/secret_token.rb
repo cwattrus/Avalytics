5402b98c9112e12cf33768834ea361306ce975fb
Patrick Turley [pturley] <patrick.turley@gmail.com>
2013-01-22 23:21:01 +0200

    Turley: initial app.  just rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Avalytics::Application.config.secret_token = '8dd28bc69c90dae261de1ce7868c5a8db405398c27d9d3ee4feb067baf428f4285e18c890ce3615a8453741bf0faf5babd17402a24344317a8feefea7772bff6'
