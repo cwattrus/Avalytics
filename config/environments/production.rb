4e165e85879179a1056b9c657b0bfb4adeec7c65
Patrick Turley [pturley] <patrick.turley@gmail.com>
2013-02-14 01:17:18 +0200

    Turley: trying to fix the asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Avalytics::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -19,8 +19,6 @@ Avalytics::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-
-  config.assets.prefix = "/assets"
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
