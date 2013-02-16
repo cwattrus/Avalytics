00a2d264c3d12bc1e7bec79a10d35c0ff19af830
Patrick Turley [pturley] <patrick.turley@gmail.com>
2013-02-17 00:04:43 +0200

    Turley: moving the cron logs to the logs directory

--- .openshift/cron/daily/ingestion.sh
+++ .openshift/cron/daily/ingestion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 pushd ${OPENSHIFT_REPO_DIR} > /dev/null
-RAILS_ENV=production rake ingest:people >> cron.log
+RAILS_ENV=production rake ingest:people >> log/cron.log
 popd > /dev/null
