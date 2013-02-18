b4ff498652134245f4471349009866f13c328fdf
Patrick Turley [pturley] <patrick.turley@gmail.com>
2013-02-19 00:41:48 +0200

    Turley: making things look a bit nicer

--- app/controllers/records_to_clean_controller.rb
+++ app/controllers/records_to_clean_controller.rb
@@ -1,4 +1,7 @@
 class RecordsToCleanController < ApplicationController
+  def index
+  end
+
   def strange_country_city_combos
     @people = Person.for_js("this.countries.length != this.cities.length || this.countries.length == 0 || this.cities.length == 0")
 
