50bcbfc36d9700a1d22b46c1744006ed15f08751
Patrick Turley [pturley] <patrick.turley@gmail.com>
2013-02-18 13:33:37 +0200

    Turley: adding logout and change password links

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Avalytics::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :skip => [:registrations]
+    as :user do
+      get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+      put 'users' => 'devise/registrations#update', :as => 'user_registration'
+    end
 
   resources :job_lists
-
 
   resources :people, :only => [:index, :show]
 
