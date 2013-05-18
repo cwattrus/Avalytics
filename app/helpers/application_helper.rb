e29d95e00ccc0743d56dc3e3bf438f9dd0d33251
Patrick Turley [pturley] <patrick.turley@gmail.com>
2013-05-18 13:50:38 +0200

    Fixing up some styles that could be more sassy and cleaning up the progress tracker code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def progress_tracker_class(person, attribute)
+    incomplete_step_class = "progress-tracker-todo"
+    incomplete_step_class = "progress-tracker-hold" if person.step == "Hold"
+    incomplete_step_class = "progress-tracker-rejected" if person.step == "Rejected"
+    person.send(attribute.to_s + "?") ? "progress-tracker-done" : incomplete_step_class
+  end
 end
